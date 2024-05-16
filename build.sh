7d90da257ae67653e83fd146da7aa1b1ad14e6f9
Jon Wedell <jon@jonwedell.com>
2024-05-16 10:27:28 -0500

    Updates to build and test scripts as well as main alphafold definition file.

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+mkdir -p singularity/cache
+mkdir -p singularity/tmp
+
+export SINGULARITY_CACHEDIR=$(pwd)/singularity/cache
+export APPTAINER_CACHEDIR=$(pwd)/singularity/cache
+export SINGULARITY_TMPDIR=$(pwd)/singularity/tmp
+export APPTAINER_TMPDIR=$(pwd)/singularity/tmp
+
+singularity_loc=$(which singularity)
 # build base container
-sudo /usr/software/bin/singularity build base.sif base.def
+"$singularity_loc" build base.sif base.def
 # build intermediate container
-sudo /usr/software/bin/singularity build conda_base_alphafold.sif conda_base_alphafold.def
+"$singularity_loc" build conda_base_alphafold.sif conda_base_alphafold.def
 # build package-specific container
-sudo /usr/software/bin/singularity build alphafold.sif alphafold.def
+"$singularity_loc" build alphafold.sif alphafold.def
 
--- test_gpu.sh
+++ test_gpu.sh
@@ -4,3 +4,4 @@ source /opt/miniconda3/etc/profile.d/conda.sh
 conda activate alphafold
 export TF_CPP_MIN_LOG_LEVEL=0
 python3 -c 'import jax; print(jax.devices())'
+python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
